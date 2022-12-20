38e1805a6a8773a5444fc7ac018405e33a76bd56
SonJava10Rikkei <sonjapan7@gmail.com>
2022-12-20 17:52:12 +0700

    a

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-let arrayImages = ['/image/tb-pc.jpg', '/image/tp-pc-2.jpg',]
+
+// thêm xổ ra danh sách fashion trong menu mobile
+
+var add_menu_fashion_mobile_add = document.getElementById("add_element_fashion_mobile");
+let count_add_element_menu = 1;
+
+add_menu_fashion_mobile_add.onclick = function () {
+    count_add_element_menu++;
+
+    let fashion_element_mobile = document.querySelector(".fashion_element_mobile");
+
+    fashion_element_mobile.style.display = "block";
+    add_menu_fashion_mobile_add.classList.add("fa-minus");
+    add_menu_fashion_mobile_add.classList.remove("fa-plus");
+
+    if (count_add_element_menu == 3) {
+        fashion_element_mobile.style.display = "none";
+        add_menu_fashion_mobile_add.classList.add("fa-plus");
+        add_menu_fashion_mobile_add.classList.remove("fa-minus");
+        count_add_element_menu = 1;
+    }
+}
+
+// banner
+let arrayImages = [
+
+    '/image/banner-tp-pc-2.jpg',
+    '/image/banner-tet.jpg',
+    '/image/banner-best-sell-50.jpg',
+    '/image/banner-offtion-buy-4.jpg',
+]
 let count = 0;
 let showBanner = function () {
     let image = document.getElementById('banner-img');
@@ -8,4 +38,5 @@ let showBanner = function () {
         count = 0;
     }
 }
-setInterval(showBanner, 4000);
+setInterval(showBanner, 3500);
+
