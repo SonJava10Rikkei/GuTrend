9e960d94e6722dd10294e910756175b03a532aea
SonJava10Rikkei <sonjapan7@gmail.com>
2022-12-23 16:27:23 +0700

    a

--- js/login.js
+++ js/login.js
@@ -8,4 +8,45 @@ eye.addEventListener("click", function () {
     this.classList.toggle("fa-eye");
 })
 
+const ipnElement = document.querySelector("#Ippassword");
+var flag = false;
 
+
+////////////////// Kiểm tra tài khoản đăng nhập /////////////////////
+let getMember = JSON.parse(localStorage.getItem("Member"));
+function checkAccount() {
+    let inputID = document.getElementById("inputID");
+    let Ippassword = document.getElementById("Ippassword");
+    if (inputID.value != "" && Ippassword.value != "") {
+        for (let i = 0; i < getMember.length; i++) {
+            if (
+                getMember[i].username == inputID.value &&
+                getMember[i].password == Ippassword.value && getMember[i].permissions === "actived"
+            ) {
+                getMember[i].status = true;
+                flag = true;
+                localStorage.setItem("Flag", JSON.stringify(flag));
+                localStorage.setItem("Member", JSON.stringify(getMember));
+                window.location = "/index.html";
+                document.getElementById("signInform").onsubmit = function (e) {
+                    e.preventDefault();
+                };
+                break;
+            } else {
+                localStorage.setItem("Flag", JSON.stringify(flag));
+                document.getElementById("checkAcc_1").innerHTML =
+                    "Sai mật khẩu hoặc tài khoản.";
+                document.getElementById("losePassword").style.display = "block";
+                document.getElementById("signInform").onsubmit = function (e) {
+                    e.preventDefault();
+                };
+            }
+        }
+    } else {
+        document.getElementById("checkAcc_2").innerHTML =
+            "Xin mời điền đầy đủ email và mật khẩu.";
+        document.getElementById("signInform").onsubmit = function (e) {
+            e.preventDefault();
+        };
+    }
+}
--- js/register.js
+++ js/register.js
@@ -17,3 +17,159 @@ eye2.addEventListener("click", function () {
 })
 
 
+const ipnElement = document.querySelector("#Ippassword");
+const ipnElement1 = document.querySelector("#checkPassword");
+
+//! Hàm kiểm tra ID theo định dạng email //
+function checkID() {
+    let inputID = document.getElementById("inputID");
+    let pattern =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    inputID.addEventListener("keyup", function () {
+        let result = pattern.test(inputID.value);
+        if (result == true) {
+            document.getElementById("valiadteID").innerHTML = "email hợp lệ";
+            document.getElementById("valiadteID").style.color = "green";
+        } else {
+            document.getElementById("valiadteID").innerHTML =
+                "ID chưa đúng định dạng Email.";
+            document.getElementById("valiadteID").style.color = "red";
+
+        }
+    });
+}
+
+//! Hàm kiểm tra mật khẩu //
+let keyPass = false;
+let keyPassConfirm = false;
+function checkPassword() {
+    let passwordInput = document.getElementById("Ippassword");
+    passwordInput.addEventListener("keyup", function () {
+        let array = [];
+        let text;
+        array.push(passwordInput.value);
+        let check = array.toString().split("");
+        if (check.length < 8) {
+            text = "Vui lòng nhập tối thiểu 8 ký tự";
+            document.getElementById("validateIppassword").innerHTML = text;
+            document.getElementById("validateIppassword").style.color = "red";
+
+            keyPass = false;
+        } else {
+            text = "";
+            document.getElementById("validateIppassword").innerHTML = "pass đủ yêu cầu";
+            document.getElementById("validateIppassword").style.color = "green";
+            keyPass = true;
+        }
+        return keyPass;
+    });
+}
+
+function confirmedPassword() {
+    let passwordInput = document.getElementById("Ippassword");
+    let confirmedPassword = document.getElementById("Ippassword2");
+    confirmedPassword.addEventListener("keyup", function () {
+        let text;
+        if (
+            passwordInput.value == confirmedPassword.value &&
+            passwordInput.value != "" &&
+            confirmedPassword.value != ""
+        ) {
+            text = "Mật khẩu đã trùng khớp";
+            keyPassConfirm = true;
+
+            document.getElementById("validateIppassword2").style.color = "green";
+        } else if (passwordInput.value == "" || confirmedPassword.value == "") {
+            text = "Mật khẩu đang bỏ trống";
+            keyPassConfirm = false;
+            document.getElementById("validateIppassword2").style.color = "red";
+        } else {
+            text = "Mật khẩu chưa trùng khớp";
+            keyPassConfirm = false;
+            document.getElementById("validateIppassword2").style.color = "red";
+
+        }
+        document.getElementById("validateIppassword2").innerHTML = text;
+        return keyPassConfirm;
+    });
+}
+
+// Kiểm tra có đăng ký được hay không //
+function registerConfirm() {
+    if ((keyPass == false || keyPassConfirm == false)) {
+        document.getElementById("signUpform").onsubmit = function (e) {
+            e.preventDefault();
+        };
+    }
+}
+
+//! Hàm thêm thành viên//
+let getMember = localStorage.getItem("Member");
+class Member {
+    constructor(username, password, status, secret, permissions, avatar, id) {
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.secret = secret;
+        this.permissions = permissions;
+        this.avatar = avatar;
+        this.id = id;
+    }
+}
+function newMember() {
+    let inputID = document.getElementById("inputID");
+    let Ippassword = document.getElementById("Ippassword");
+    if (
+        inputID.value != "" &&
+        Ippassword.value != "" &&
+        keyPassConfirm == true
+    ) {
+        if (getMember == null) {
+            let MemberList = [];
+            let newMem = new Member(
+                inputID.value,
+                Ippassword.value,
+                false,
+                "actived",
+                "/image/user.png",
+                MemberList.length + 1
+            );
+            MemberList.push(newMem);
+            localStorage.setItem("Member", JSON.stringify(MemberList));
+            alert("Đăng ký thành công.");
+            window.location = "./login.html";
+        } else {
+            MemberList = JSON.parse(getMember);
+            let keyAcc = true;
+            for (i = 0; i < MemberList.length; i++) {
+                if (MemberList[i].username == inputID.value) {
+                    keyAcc = false;
+                    break;
+                }
+            }
+            if (keyAcc) {
+                a = new Member(
+                    inputID.value,
+                    Ippassword.value,
+                    false,
+                    secretQuestion.value,
+                    "actived",
+                    "/image/user.png",
+                    MemberList.length + 1
+                );
+                MemberList.push(a);
+                console.log(MemberList);
+                localStorage.setItem("Member", JSON.stringify(MemberList));
+                alert("Đăng ký thành công.");
+                window.location = "./login.html";
+            } else {
+                alert("Tài khoản đã tồn tại !!!");
+            }
+        }
+    }
+    return;
+}
+checkID();
+checkPassword();
+confirmedPassword();
+registerConfirm();
